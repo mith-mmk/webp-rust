6a2c12b7ae182bdfc6c89febb08452b69f7efe01
MITH@mmk <47620686+mith-mmk@users.noreply.github.com>
2026-03-10 20:17:53 +0900

    sample path changes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ type Error = Box<dyn std::error::Error>;
 
 
 pub fn main() -> Result<(),Error>{
-    let mut file = File::open("/tools/riff/image.webp")?;
+    let mut file = File::open("./_testdata/sample.webp")?;
     let mut buf = vec![];
     file.read_to_end(&mut buf)?;
     let mut reader = bin_rs::reader::BytesReader::from_vec(buf);
